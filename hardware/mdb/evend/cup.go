6664392e330c412b60cdfe1d6c10b28100214d2d
Alexey Milko <alexey.milko@gmail.com>
2022-06-07 18:02:35 +0500

    --

--- hardware/mdb/evend/cup.go
+++ hardware/mdb/evend/cup.go
@@ -3,11 +3,9 @@ package evend
 import (
 	"context"
 	"fmt"
-
-	// "go/types"
 	"time"
 
-	"github.com/AlexTransit/vender/hardware/mdb"
+	// "github.com/AlexTransit/vender/hardware/mdb"
 	"github.com/AlexTransit/vender/helpers"
 	"github.com/AlexTransit/vender/internal/engine"
 	"github.com/AlexTransit/vender/internal/state"
@@ -16,7 +14,7 @@ import (
 )
 
 const DefaultCupAssertBusyDelay = 30 * time.Millisecond
-const DefaultCupDispenseTimeout = 10 * time.Second
+const DefaultCupDispenseTimeout = 20 * time.Second
 const DefaultCupEnsureTimeout = 70 * time.Second
 
 type DeviceCup struct {
@@ -29,13 +27,13 @@ func (devCup *DeviceCup) init(ctx context.Context) error {
 	devCup.Generic.Init(ctx, 0xe0, "cup", proto2)
 
 	g := state.GetGlobal(ctx)
-	doDispense := devCup.Generic.WithRestart(devCup.NewDispenseProper())
-	g.Engine.Register(devCup.name+".dispense", doDispense)
+	devCup.dispenseTimeout = helpers.IntSecondDefault(g.Config.Hardware.Evend.Cup.DispenseTimeoutSec, DefaultCupDispenseTimeout)
+	devCup.assertBusyDelayMils = helpers.IntMillisecondDefault(g.Config.Hardware.Evend.Cup.AssertBusyDelayMs, DefaultCupAssertBusyDelay)
+	// doDispense := devCup.Generic.WithRestart(devCup.NewDispenseProper())
+	g.Engine.Register(devCup.name+".dispense", devCup.WithRestart(devCup.NewDispenseProper()))
 	g.Engine.Register(devCup.name+".light_on", devCup.NewLight(true))
 	g.Engine.Register(devCup.name+".light_off", devCup.NewLight(false))
 	g.Engine.Register(devCup.name+".ensure", devCup.NewEnsure())
-	devCup.dispenseTimeout = helpers.IntSecondDefault(g.Config.Hardware.Evend.Cup.DispenseTimeoutSec, DefaultCupDispenseTimeout)
-	devCup.assertBusyDelayMils = helpers.IntMillisecondDefault(g.Config.Hardware.Evend.Cup.AssertBusyDelayMs, DefaultCupAssertBusyDelay)
 
 	err := devCup.Generic.FIXME_initIO(ctx)
 	return errors.Annotate(err, devCup.name+".init")
@@ -43,7 +41,7 @@ func (devCup *DeviceCup) init(ctx context.Context) error {
 
 func (devCup *DeviceCup) NewDispenseProper() engine.Doer {
 	return engine.NewSeq(devCup.name + ".dispense_proper").
-		Append(devCup.NewEnsure()).
+		// Append(devCup.NewEnsure()).
 		Append(devCup.NewDispense())
 }
 
@@ -51,32 +49,33 @@ func (devCup *DeviceCup) NewDispense() engine.Doer {
 	tag := devCup.name + ".dispense"
 	return engine.NewSeq(tag).
 		Append(engine.Func0{F: func() error { types.Log.Info("cup dispence"); return nil }}).
-		Append(devCup.Generic.NewWaitReady(tag)).
-		Append(devCup.Generic.NewAction(tag, 0x01)).
-		Append(engine.Func{Name: tag + "/assert-busy", F: func(ctx context.Context) error {
-			time.Sleep(devCup.assertBusyDelayMils)
-			response := mdb.Packet{}
-			err := devCup.dev.TxKnown(devCup.dev.PacketPoll, &response)
-			if err != nil {
-				return err
-			}
-			bs := response.Bytes()
-			if len(bs) != 1 {
-				return devCup.NewErrPollUnexpected(response)
-			}
-			if bs[0] != devCup.proto2BusyMask {
-				devCup.dev.Log.Errorf("expected BUSY, cup device is broken")
-				return devCup.NewErrPollUnexpected(response)
-			}
-			return nil
-		}}).
-		Append(engine.Func{
-			F: func(ctx context.Context) error {
-				g := state.GetGlobal(ctx)
-				return g.Engine.Exec(ctx, devCup.Generic.NewWaitDone(tag, devCup.dispenseTimeout))
-			},
-		})
+		Append(devCup.NewWaitReady(tag)).
+		Append(devCup.NewAction(tag, 0x01)).
+		Append(devCup.NewWaitDone(tag, devCup.dispenseTimeout))
 }
+
+// func (devCup *DeviceCup) aa() engine.Doer {
+// 	return engine.NewSeq("blablabla").
+// 	Append(engine.Func{Name: "/assert-busy", F: func(ctx context.Context) error {
+// 		// time.Sleep(devCup.assertBusyDelayMils)
+// 		time.Sleep(400 * time.Millisecond)
+// 		response := mdb.Packet{}
+// 		err := devCup.dev.TxKnown(devCup.dev.PacketPoll, &response)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		bs := response.Bytes()
+// 		fmt.Printf("\033[41m %v \033[0m\n", bs)
+// 		if len(bs) != 1 {
+// 			return devCup.NewErrPollUnexpected(response)
+// 		}
+// 		if bs[0] != devCup.proto2BusyMask {
+// 			devCup.dev.Log.Errorf("expected BUSY, cup device is broken")
+// 			return devCup.NewErrPollUnexpected(response)
+// 		}
+// 		return nil
+// 	}})
+// }
 
 func (devCup *DeviceCup) NewLight(v bool) engine.Doer {
 	tag := fmt.Sprintf("%s.light:%t", devCup.name, v)
@@ -84,10 +83,8 @@ func (devCup *DeviceCup) NewLight(v bool) engine.Doer {
 	if !v {
 		arg = 0x03
 	}
-	types.SetLight(v)
-	// return devCup.Generic.NewAction(tag, arg)
 	return engine.NewSeq(tag).
-		Append(devCup.Generic.NewAction(tag, arg)).
+		Append(devCup.NewAction(tag, arg)).
 		Append(engine.Func0{F: func() error { types.SetLight(v); return nil }})
 
 }
@@ -95,14 +92,15 @@ func (devCup *DeviceCup) NewLight(v bool) engine.Doer {
 func (devCup *DeviceCup) NewEnsure() engine.Doer {
 	tag := devCup.name + ".ensure"
 	return engine.NewSeq(tag).
-		Append(devCup.Generic.NewWaitReady(tag)).
-		Append(devCup.Generic.NewAction(tag, 0x04)).
-		Append(engine.Func{
-			F: func(ctx context.Context) error {
-				g := state.GetGlobal(ctx)
-				cupConfig := &g.Config.Hardware.Evend.Cup
-				ensureTimeout := helpers.IntSecondDefault(cupConfig.EnsureTimeoutSec, DefaultCupEnsureTimeout)
-				return g.Engine.Exec(ctx, devCup.Generic.NewWaitDone(tag, ensureTimeout))
-			},
-		})
+		Append(devCup.NewWaitReady(tag)).
+		Append(devCup.NewAction(tag, 0x04)).
+		Append(devCup.NewWaitDone(tag, devCup.dispenseTimeout))
+	// Append(engine.Func{
+	// 	F: func(ctx context.Context) error {
+	// 		g := state.GetGlobal(ctx)
+	// 		cupConfig := &g.Config.Hardware.Evend.Cup
+	// 		ensureTimeout := helpers.IntSecondDefault(cupConfig.EnsureTimeoutSec, DefaultCupEnsureTimeout)
+	// 		return g.Engine.Exec(ctx, devCup.Generic.NewWaitDone(tag, ensureTimeout))
+	// 	},
+	// })
 }
