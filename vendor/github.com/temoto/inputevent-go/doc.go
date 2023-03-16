27ae184b5b4cd4e1798344efd8f92bca5d87b218
Alexey Milko <alexey.milko@gmail.com>
2023-03-16 12:24:14 +0500

    upgrade vendor

--- vendor/github.com/temoto/inputevent-go/doc.go
+++
@@ -1,4 +0,0 @@
-// This is stripped down version of github.com/gvalkov/golang-evdev
-// Removed OS and CGo dependencies.
-// Input events parsed from io.Reader, you probably want to use `os.Open("/dev/input/...")`.
-package inputevent
--- vendor/github.com/temoto/inputevent-go/readme.md
+++
@@ -1,8 +0,0 @@
-What
-====
-
-InputEvent parser and constants in pure Go.
-
-Copied from https://github.com/gvalkov/golang-evdev
-
-This stripped down version does not access OS and does not require CGo.
