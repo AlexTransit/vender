941a8fdc88f6306d7ee4882b9c623cc0d77381e0
Alexey Milko <alexey.milko@gmail.com>
2023-06-10 16:00:55 +0500

    mdb tx change

--- hardware/mdb/client/io_mega.go
+++ hardware/mdb/client/io_mega.go
@@ -1,6 +1,7 @@
 package mdb_client
 
 import (
+	"fmt"
 	"sync"
 	"time"
 
@@ -80,37 +81,22 @@ func (mu *megaUart) Tx(request, response []byte) (int, error) {
 	const tag = "mdb.mega.Tx"
 	mu.lk.Lock()
 	defer mu.lk.Unlock()
-
 	var f mega.Frame
 	var err error
-	for retry := 1; retry <= 3; retry++ {
-		// FIXME should not be here, but fixes MDB busy/uart_unexpected race
-		time.Sleep(10 * time.Millisecond)
-
-		f, err = mu.c.DoMdbTxSimple(request)
-		switch errors.Cause(err) {
-		case nil: // success path
-			mu.c.Log.Debugf("%s request=%x f=%s", tag, request, f.ResponseString())
-			err = responseError(f.Fields.MdbResult, f.Fields.MdbError)
-			if err == nil {
-				n := copy(response, f.Fields.MdbData)
-				return n, nil
-			}
-			time.Sleep(DelayErr)
-
-		case mega.ErrCriticalProtocol:
-			// Alexm - падает все. бабло не возвращает.
-			err = errors.Annotatef(err, "%s CRITICAL request=%x", tag, request)
-			mu.c.Log.Error(err)
-			return 0, err
-
-		default:
-			err = errors.Annotatef(err, "%s request=%x", tag, request)
-			mu.c.Log.Error(err)
-			return 0, err
-		}
+	f, err = mu.c.DoMdbTxSimple(request)
+	switch errors.Cause(err) {
+	case nil: // success path
+		mu.c.Log.Debugf("%s request=%x f=%s", tag, request, f.ResponseString())
+		n := copy(response, f.Fields.MdbData)
+		return n, nil
+	case mega.ErrCriticalProtocol:
+		// Alexm - падает все. бабло не возвращает.
+		err = errors.Annotatef(err, "%s CRITICAL request=%x", tag, request)
+		mu.c.Log.Error(err)
+		return 0, err
+	default:
+		err = errors.Annotatef(err, "%s request=%x", tag, request)
+		mu.c.Log.Error(err)
+		return 0, err
 	}
-	mu.c.Log.WarningF("mega TX error 3 times (%v)", err)
-	// return 0, err
-	return 0, nil
 }
