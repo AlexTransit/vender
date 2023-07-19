61f8caffadcb286873dd29dc646dd923625bba04
Alexey Milko <alexey.milko@gmail.com>
2023-07-19 18:31:41 +0500

    change espresso

--- hardware/mdb/evend/espresso.go
+++ hardware/mdb/evend/espresso.go
@@ -2,108 +2,90 @@ package evend
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/AlexTransit/vender/helpers"
-	"github.com/AlexTransit/vender/internal/engine"
 	"github.com/AlexTransit/vender/internal/state"
 	"github.com/juju/errors"
 )
 
-const DefaultEspressoTimeout = 30 * time.Second
+const defaultEspressoTimeout = 30
 
 type DeviceEspresso struct {
 	Generic
 
-	timeout time.Duration
+	timeout uint8
 }
 
 func (d *DeviceEspresso) init(ctx context.Context) error {
 	g := state.GetGlobal(ctx)
 	espressoConfig := &g.Config.Hardware.Evend.Espresso
-	d.timeout = helpers.IntSecondDefault(espressoConfig.TimeoutSec, DefaultEspressoTimeout)
+	d.timeout = uint8(helpers.IntConfigDefault(espressoConfig.TimeoutSec, defaultEspressoTimeout)) * 5 //every 200 ms
 	d.Generic.Init(ctx, 0xe8, "espresso", proto2)
-
-	g.Engine.Register(d.name+".grind", engine.Func{F: func(ctx context.Context) (err error) {
-		for i := 0; i < 3; i++ {
-			if err = g.Engine.Exec(ctx, d.grind()); err == nil {
-				if i != 0 {
-					d.dev.TeleError(errors.Errorf("restart fix preview error"))
-				}
-				return nil
-			}
-			d.dev.TeleError(err)
-			d.dev.Reset()
-		}
-		d.dev.TeleError(errors.Annotatef(err, "many times error"))
-		return err
-	}})
-	g.Engine.Register(d.name+".grindNoWait", d.GrindNoWait())
-	g.Engine.Register(d.name+".waitDone", d.WaitDone())
-	g.Engine.Register(d.name+".press", d.NewPress())
-	g.Engine.Register(d.name+".dispose", d.NewRelease())
-	g.Engine.Register(d.name+".heat_on", d.NewHeat(true))
-	g.Engine.Register(d.name+".heat_off", d.NewHeat(false))
+	g.Engine.RegisterNewFunc(d.name+".waitDone", func(ctx context.Context) error { return d.Proto2PollWaitSuccess(100) })
+	g.Engine.RegisterNewFunc(d.name+".grindNoWait", func(ctx context.Context) error { return d.grindNoWait() })
+	g.Engine.RegisterNewFunc(d.name+".grind", func(ctx context.Context) error { return d.grind() })
+	g.Engine.RegisterNewFunc(d.name+".pressNoWait", func(ctx context.Context) error { return d.pressNoWait() })
+	g.Engine.RegisterNewFunc(d.name+".press", func(ctx context.Context) error { return d.press() })
+	g.Engine.RegisterNewFunc(d.name+".disposeNoWait", func(ctx context.Context) error { return d.releaseNoWait() })
+	g.Engine.RegisterNewFunc(d.name+".dispose", func(ctx context.Context) error { return d.release() })
+	g.Engine.RegisterNewFunc(d.name+".heat_on", func(ctx context.Context) error { return d.heatOn() })
+	g.Engine.RegisterNewFunc(d.name+".heat_off", func(ctx context.Context) error { return d.heatOff() })
 
 	err := d.dev.Rst()
+	time.Sleep(200 * time.Millisecond)
 	return errors.Annotate(err, d.name+".init")
 }
 
-func (d *DeviceEspresso) grind() engine.Doer {
-	tag := d.name + ".grind"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewAction(tag, 0x01)).
-		// TODO expect delay like in cup dispense, ignore immediate error, retry
-		Append(d.Generic.NewWaitDone(tag, d.timeout))
-}
-
-func (d *DeviceEspresso) NewGrind() engine.Doer {
-	tag := d.name + ".grind"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewAction(tag, 0x01)).
-		// TODO expect delay like in cup dispense, ignore immediate error, retry
-		Append(d.Generic.NewWaitDone(tag, d.timeout))
-}
-
-func (d *DeviceEspresso) GrindNoWait() engine.Doer {
-	tag := d.name + ".grindNoWait"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewAction(tag, 0x01))
-}
-
-func (d *DeviceEspresso) WaitDone() engine.Doer {
-	tag := d.name + ".waitDone"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewWaitDone(tag, d.timeout))
-}
-
-func (d *DeviceEspresso) NewPress() engine.Doer {
-	tag := d.name + ".press"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewAction(tag, 0x02)).
-		Append(d.Generic.NewWaitDone(tag, d.timeout))
-}
-
-func (d *DeviceEspresso) NewRelease() engine.Doer {
-	tag := d.name + ".release"
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewAction(tag, 0x03)).
-		Append(d.Generic.NewWaitDone(tag, d.timeout))
-}
-
-func (d *DeviceEspresso) NewHeat(on bool) engine.Doer {
-	tag := fmt.Sprintf("%s.heat:%t", d.name, on)
-	arg := byte(0x05)
-	if !on {
-		arg = 0x06
+func (d *DeviceEspresso) pollBeforeAfter(f func()) error {
+	if err := d.Proto2PollWaitSuccess(5); err != nil {
+		return err
 	}
-	return engine.NewSeq(tag).
-		Append(d.Generic.NewWaitReady(tag)).
-		Append(d.Generic.NewAction(tag, arg))
+	f()
+	return d.Proto2PollWaitSuccess(5)
+}
+
+func (d *DeviceEspresso) cmd(byteCmd byte) error {
+	return d.pollBeforeAfter(func() { d.Command([]byte{byteCmd}) })
+}
+
+func (d *DeviceEspresso) grindNoWait() error {
+	return d.cmd(0x01)
+}
+
+func (d *DeviceEspresso) grind() (err error) {
+	if err = d.grindNoWait(); err != nil {
+		return
+	}
+	return d.Proto2PollWaitSuccess(d.timeout)
+}
+
+func (d *DeviceEspresso) pressNoWait() error {
+	return d.cmd(0x02)
+}
+
+func (d *DeviceEspresso) press() (err error) {
+	if err = d.pressNoWait(); err != nil {
+		return
+	}
+	return d.Proto2PollWaitSuccess(d.timeout)
+}
+
+func (d *DeviceEspresso) releaseNoWait() error {
+	return d.cmd(0x03)
+}
+
+func (d *DeviceEspresso) release() (err error) {
+	if err = d.releaseNoWait(); err != nil {
+		return
+	}
+	return d.Proto2PollWaitSuccess(d.timeout)
+}
+
+func (d *DeviceEspresso) heatOn() error {
+	return d.cmd(0x05)
+}
+
+func (d *DeviceEspresso) heatOff() error {
+	return d.cmd(0x06)
 }
