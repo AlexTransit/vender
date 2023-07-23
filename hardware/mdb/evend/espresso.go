d791548c13b0ac3fa87c2ef3c002f85d1be7b8e2
Alexey Milko <alexey.milko@gmail.com>
2023-07-23 13:17:27 +0500

    --

--- hardware/mdb/evend/espresso.go
+++ hardware/mdb/evend/espresso.go
@@ -19,10 +19,9 @@ type DeviceEspresso struct {
 
 func (d *DeviceEspresso) init(ctx context.Context) error {
 	g := state.GetGlobal(ctx)
-	espressoConfig := &g.Config.Hardware.Evend.Espresso
-	d.timeout = uint8(helpers.IntConfigDefault(espressoConfig.TimeoutSec, defaultEspressoTimeout)) * 5 //every 200 ms
+	d.timeout = uint8(helpers.IntConfigDefault(g.Config.Hardware.Evend.Espresso.TimeoutSec, defaultEspressoTimeout)) * 5 //every 200 ms
 	d.Generic.Init(ctx, 0xe8, "espresso", proto2)
-	g.Engine.RegisterNewFunc(d.name+".waitDone", func(ctx context.Context) error { return d.Proto2PollWaitSuccess(d.timeout) })
+	g.Engine.RegisterNewFunc(d.name+".waitDone", func(ctx context.Context) error { return d.Proto2PollWaitSuccess(d.timeout, true) })
 	g.Engine.RegisterNewFunc(d.name+".grindNoWait", func(ctx context.Context) error { return d.grindNoWait() })
 	g.Engine.RegisterNewFunc(d.name+".grind", func(ctx context.Context) error { return d.grind() })
 	g.Engine.RegisterNewFunc(d.name+".pressNoWait", func(ctx context.Context) error { return d.pressNoWait() })
@@ -37,39 +36,29 @@ func (d *DeviceEspresso) init(ctx context.Context) error {
 	return errors.Annotate(err, d.name+".init")
 }
 
-func (d *DeviceEspresso) grindNoWait() error   { return d.cmd(0x01) }
-func (d *DeviceEspresso) pressNoWait() error   { return d.cmd(0x02) }
-func (d *DeviceEspresso) releaseNoWait() error { return d.cmd(0x03) }
-func (d *DeviceEspresso) heatOn() error        { return d.cmd(0x05) }
-func (d *DeviceEspresso) heatOff() error       { return d.cmd(0x06) }
-
-func (d *DeviceEspresso) cmd(byteCmd byte) (err error) {
-	if err = d.Proto2PollWaitSuccess(5); err != nil {
-		return err
-	}
-	if err = d.Command([]byte{byteCmd}); err != nil {
-		return errors.Errorf("command transmit error (%v)", err)
-	}
-	return d.Proto2PollWaitSuccess(5)
-}
+func (d *DeviceEspresso) grindNoWait() error   { return d.CommandNoWait(0x01) }
+func (d *DeviceEspresso) pressNoWait() error   { return d.CommandNoWait(0x02) }
+func (d *DeviceEspresso) releaseNoWait() error { return d.CommandNoWait(0x03) }
+func (d *DeviceEspresso) heatOn() error        { return d.CommandNoWait(0x05) }
+func (d *DeviceEspresso) heatOff() error       { return d.CommandNoWait(0x06) }
 
 func (d *DeviceEspresso) grind() (err error) {
 	if err = d.grindNoWait(); err != nil {
 		return
 	}
-	return d.Proto2PollWaitSuccess(d.timeout)
+	return d.WaitSuccess(d.timeout, true)
 }
 
 func (d *DeviceEspresso) press() (err error) {
 	if err = d.pressNoWait(); err != nil {
 		return
 	}
-	return d.Proto2PollWaitSuccess(d.timeout)
+	return d.WaitSuccess(d.timeout, true)
 }
 
 func (d *DeviceEspresso) release() (err error) {
 	if err = d.releaseNoWait(); err != nil {
 		return
 	}
-	return d.Proto2PollWaitSuccess(d.timeout)
+	return d.WaitSuccess(d.timeout, true)
 }
