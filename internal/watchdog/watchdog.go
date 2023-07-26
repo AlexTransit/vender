17ec3b43b6254d715f669f345b50124252ce39af
Alexey Milko <alexey.milko@gmail.com>
2023-07-26 18:48:46 +0500

    wd modify

--- internal/watchdog/watchdog.go
+++ internal/watchdog/watchdog.go
@@ -3,6 +3,7 @@ package watchdog
 import (
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/AlexTransit/vender/internal/types"
 )
@@ -23,6 +24,19 @@ func WatchDogEnable() {
 	// 	types.Log.Errorf("cant change heartBeatFile owner (%v)", err)
 	// }
 
+	createWatchDogFile()
+	b, err := os.ReadFile(heartBeatFile)
+	hbfd := string(b)
+	if err != nil || hbfd != wdTics {
+		types.Log.Errorf("error check watchdog heartBeatFile read data:%v error:%v", hbfd, err)
+		go func() {
+			time.Sleep(1 * time.Second)
+			createWatchDogFile()
+		}()
+	}
+
+}
+func createWatchDogFile() {
 	f, err := os.Create(heartBeatFile)
 	_ = err
 	if _, err := f.WriteString(wdTics); err != nil {
