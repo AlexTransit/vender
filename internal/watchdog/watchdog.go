39bcb798f4d67441aec8c82ddac777dad9bc8bd4
Alexey Milko <alexey.milko@gmail.com>
2024-01-07 11:31:03 +0500

    --

--- internal/watchdog/watchdog.go
+++ internal/watchdog/watchdog.go
@@ -10,11 +10,13 @@ import (
 )
 
 type Config struct {
-	Folder string
+	Disabled bool
+	Folder   string
 }
 type wdStruct struct {
-	Folder string
-	log    *log2.Log
+	Disabled bool
+	Folder   string
+	log      *log2.Log
 	// hbf string
 	wdt string
 }
@@ -26,6 +28,7 @@ var WD wdStruct
 func Init(conf *Config, log *log2.Log) {
 	WD.Folder = helpers.ConfigDefaultStr(conf.Folder, "/run/user/1000/")
 	WD.log = log
+	WD.Disabled = conf.Disabled
 }
 
 func WatchDogEnable() {
@@ -49,7 +52,7 @@ func WatchDogEnable() {
 }
 
 func createWatchDogFile() {
-	f, err := os.Create(WD.Folder + "hb")
+	f, err := os.Create(WD.Folder + file)
 	_ = err
 	if _, err := f.WriteString(WD.wdt); err != nil {
 		WD.log.Errorf("error create watchdog heartBeatFile (%v)", err)
@@ -61,12 +64,15 @@ func createWatchDogFile() {
 
 func WatchDogDisable() {
 	WD.log.Notice("watchdog disabled.")
-	if err := os.Remove(WD.Folder + "hb"); err != nil {
+	if err := os.Remove(WD.Folder + file); err != nil {
 		WD.log.Errorf("delete heartBeatFile error(%v)", err)
 	}
 }
 
 func WatchDogSetTics(tics int) {
+	if WD.Disabled {
+		return
+	}
 	WD.wdt = strconv.Itoa(tics)
 	WD.log.Infof("watchdog set count:%d", tics)
 }
