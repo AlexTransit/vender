6993a1e2e02118bcd09d6f4ae2ef91376834d73f
Alexey Milko <alexey.milko@gmail.com>
2025-03-31 15:34:46 +0500

    fix bill escrow

--- hardware/mdb/bill/bill.go
+++ hardware/mdb/bill/bill.go
@@ -510,12 +510,12 @@ func (bv *BillValidator) BillRun(alive *alive.Alive, returnEvent func(money.Vali
 	for again {
 		select {
 		case <-stopRun:
-			bv.billStop(returnEvent)
+			bv.billStop()
 			again = false
 		case cmd = <-bv.billCmd:
 			switch cmd {
 			case Stop:
-				bv.billStop(returnEvent)
+				bv.billStop()
 				again = false
 			case Accept:
 				returnEvent(bv.escrowAccept())
@@ -535,10 +535,11 @@ func (bv *BillValidator) BillRun(alive *alive.Alive, returnEvent func(money.Vali
 	refreshTimer.Stop()
 }
 
-func (bv *BillValidator) billStop(returnEvent func(money.ValidatorEvent)) {
+func (bv *BillValidator) billStop() {
 	bv.disableAccept()
 	if bv.EscrowBill > 0 {
 		bv.escrowReject()
+		bv.EscrowBill = 0
 	}
 	bv.setState(WaitConfigure)
 }
