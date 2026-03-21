a63c3e14edc31090488c19c43402da443e381deb
Alexey Milko <alexey.milko@gmail.com>
2026-03-21 08:15:39 +0500

    fix config

--- internal/state/new/state-new.go
+++ internal/state/new/state-new.go
@@ -8,12 +8,9 @@ import (
 
 	"github.com/AlexTransit/vender/hardware/mdb"
 	config_global "github.com/AlexTransit/vender/internal/config"
-	engine_config "github.com/AlexTransit/vender/internal/engine/config"
 	"github.com/AlexTransit/vender/internal/engine"
 	"github.com/AlexTransit/vender/internal/engine/inventory"
-	menu_config "github.com/AlexTransit/vender/internal/menu/menu_config"
 	"github.com/AlexTransit/vender/internal/state"
-	ui_config "github.com/AlexTransit/vender/internal/ui/config"
 	"github.com/AlexTransit/vender/log2"
 	tele_api "github.com/AlexTransit/vender/tele"
 	"github.com/hashicorp/hcl/v2/hclsimple"
@@ -70,60 +67,5 @@ func NewTestContext(t testing.TB, buildVersion string, confString string) (conte
 }
 
 func testConfig() *config_global.Config {
-	cfg := config_global.VMC
-	cfg.Inventory.XXX_Stocks = copyStockMap(config_global.VMC.Inventory.XXX_Stocks)
-	cfg.Inventory.XXX_Ingredient = copyIngredientMap(config_global.VMC.Inventory.XXX_Ingredient)
-	cfg.Hardware.EvendDevices = copyDeviceMap(config_global.VMC.Hardware.EvendDevices)
-	cfg.UI_config.Service.Tests = copyUITestMap(config_global.VMC.UI_config.Service.Tests)
-	cfg.Engine.Aliases = copyAliasMap(config_global.VMC.Engine.Aliases)
-	cfg.Engine.Menu.Items = copyMenuItemMap(config_global.VMC.Engine.Menu.Items)
-	return &cfg
-}
-
-func copyStockMap(in map[string]inventory.Stock) map[string]inventory.Stock {
-	out := make(map[string]inventory.Stock, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func copyIngredientMap(in map[string]inventory.Ingredient) map[string]inventory.Ingredient {
-	out := make(map[string]inventory.Ingredient, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func copyDeviceMap(in map[string]config_global.DeviceConfig) map[string]config_global.DeviceConfig {
-	out := make(map[string]config_global.DeviceConfig, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func copyUITestMap(in map[string]ui_config.TestsStruct) map[string]ui_config.TestsStruct {
-	out := make(map[string]ui_config.TestsStruct, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func copyAliasMap(in map[string]engine_config.Alias) map[string]engine_config.Alias {
-	out := make(map[string]engine_config.Alias, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
-}
-
-func copyMenuItemMap(in map[string]menu_config.MenuItem) map[string]menu_config.MenuItem {
-	out := make(map[string]menu_config.MenuItem, len(in))
-	for k, v := range in {
-		out[k] = v
-	}
-	return out
+	return config_global.NewConfig()
 }
